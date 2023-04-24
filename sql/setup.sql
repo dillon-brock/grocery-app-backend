917d3f2e589d5bc47e5c60526259462caa710449
Dillon Brock <mdillon1297@gmail.com>
2023-04-24 16:02:56 -0700

    added sql to setup a few of the main tables, no seed data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,36 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS list_items;
+DROP TABLE IF EXISTS users_lists;
+DROP TABLE IF EXISTS lists;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR,
+  password_hash VARCHAR,
+  username VARCHAR;
+);
+
+CREATE TABLE lists (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  owner_id BIGINT NOT NULL,
+  FOREIGN KEY (owner_id) REFERENCES users(id);
+);
+
+CREATE TABLE users_lists (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  list_id BIGINT,
+  FOREIGN KEY (list_id) REFERENCES lists(id),
+  FOREIGN KEY (user_id) REFERENCES users(id);
+);
+
+CREATE TABLE list_items (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  list_id BIGINT NOT NULL,
+  bought BOOLEAN NOT NULL DEFAULT FALSE,
+  quantity VARCHAR,
+  FOREIGN KEY (list_id) REFERENCES lists(id);
+);
