3d32f7cc9c0b586722f2a44560a9e56986dfdc96
Dillon Brock <mdillon1297@gmail.com>
2023-04-29 01:02:11 -0700

    wrote test for get lists/:id

--- lib/routes/lists.ts
+++ lib/routes/lists.ts
@@ -6,9 +6,16 @@ import { List } from '../models/List.js';
 export default Router()
   .post('/', authenticate, async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-      List.create(req.user.id);
-      res.json({ message: 'List successfully created' });
+      const newList: List = await List.create(req.user.id);
+      res.json({ 
+        message: 'List successfully created',
+        list: newList
+      });
     } catch (e) {
       next(e);
     }
+  })
+  .get('/:id', async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    
   });
+
--- tests/lists.test.ts
+++ tests/lists.test.ts
@@ -8,25 +8,55 @@ const testUser = {
   username: 'test_user'
 };
 
+const signUpAndGetInfo = async () => {
+  const agent = request.agent(app);
+
+  const signUpRes = await agent.post('/users').send(testUser);
+  const { token } = signUpRes.body;
+  const getUserRes = await agent.get('/users/me').set('Authorization', `Bearer ${token}`);
+  const { user } = getUserRes.body;
+
+  return { agent, token, user };
+};
+
 describe('list route tests', () => {
   beforeEach(setupDb);
 
   it('creates a new list on POST /lists', async () => {
-    const agent = request.agent(app);
 
-    // sign up user
-    const signUpRes = await agent.post('/users').send(testUser);
-    const { token } = signUpRes.body;
-    // get user info to get id
-    const getUserRes = await agent.get('/users/me').set('Authorization', `Bearer ${token}`);
-    const { user } = getUserRes.body;
+    const { agent, token, user } = await signUpAndGetInfo();
 
     const res = await agent
       .post('/lists')
-      .set('Authorization', `Breaker ${token}`)
-      .send(user.id);
+      .set('Authorization', `Breaker ${token}`);
 
     expect(res.status).toBe(200);
-    expect(res.body).toEqual({ message: 'List successfully created' });
+    expect(res.body).toEqual({ 
+      message: 'List successfully created', 
+      list: {
+        id: expect.any(String),
+        ownerId: user.id
+      } 
+    });
+  });
+
+  it('serves a list with items at GET /lists/:id', async () => {
+    
+    const { agent, user, token } = await signUpAndGetInfo();
+
+    const listPostRes = await agent
+      .post('/lists')
+      .set('Authorization', `Bearer ${token}`);
+    const listId = listPostRes.body.list.id;
+
+    const res = await agent.get(`/lists/${listId}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: 'List found',
+      list: expect.objectContaining({
+        id: expect.any(String),
+        ownerId: user.id
+      })
+    });
   });
 });
