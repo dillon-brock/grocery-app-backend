f2736f3f0a843a5ad0146fba8c01ff523204ee72
Dillon Brock <mdillon1297@gmail.com>
2023-04-29 21:49:42 -0700

    wrote test for route to update list item

--- tests/list-items.test.ts
+++ tests/list-items.test.ts
@@ -13,29 +13,45 @@ const testItem = {
   quantity: 3,
 };
 
-const signUpAndGetInfo = async () => {
+async function signUpAndGetInfo() {
   const agent = request.agent(app);
-
+  
   const signUpRes = await agent.post('/users').send(testUser);
   const { token } = signUpRes.body;
   const getUserRes = await agent.get('/users/me').set('Authorization', `Bearer ${token}`);
   const { user } = getUserRes.body;
-
+  
   return { agent, token, user };
-};
+}
+
+async function createList(agent: request.SuperAgentTest, token: string): Promise<string> {
+  
+  const newListRes = await agent
+    .post('/lists')
+    .set('Authorization', `Bearer ${token}`);
+  
+  return newListRes.body.list.id;
+
+}
+
+async function getNewItemId(agent: request.SuperAgentTest, token: string, listId: string): Promise<string> {
+
+  const newItemRes = await agent
+    .post('/list-items')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ ...testItem, listId });
+  
+  return newItemRes.body.item.id;
+
+}
 
 describe('list item routes tests', () => {
   beforeEach(setupDb);
 
   test('it adds an item to a list at POST /list-items', async () => {
     const { agent, token } = await signUpAndGetInfo();
+    const listId = await createList(agent, token);
 
-    const newListRes = await agent
-      .post('/lists')
-      .set('Authorization', `Bearer ${token}`);
-
-    const listId = newListRes.body.list.id;
-    
     const res = await agent
       .post('/list-items')
       .set('Authorization', `Bearer ${token}`)
@@ -47,5 +63,25 @@ describe('list item routes tests', () => {
       item: expect.objectContaining({ ...testItem })
     });
 
+  });
+
+  test('it updates an item at PUT /list-items/:id', async () => {
+    const { agent, token } = await signUpAndGetInfo();
+    const listId = await createList(agent, token);
+    const newItemId = await getNewItemId(agent, token, listId);
+
+    const res = await agent
+      .put(`/list-items/${newItemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ bought: true });
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: 'Item updated successfully',
+      item: expect.objectContaining({
+        ...testItem,
+        bought: true
+      })
+    });
   });
 });
