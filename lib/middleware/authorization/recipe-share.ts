a199d2b9d3ca3fde824fca557623551bb384db06
Dillon Brock <mdillon1297@gmail.com>
2023-05-09 10:04:39 -0700

    passing tests for error cases on POST /recipe-shares

--- lib/middleware/authorization/recipe-share.ts
+++ lib/middleware/authorization/recipe-share.ts
@@ -3,6 +3,7 @@ import { AuthenticatedReqBody } from '../../types/extendedExpress.js';
 import { NewRecipeShareData } from '../../types/recipeShare.js';
 import { Recipe } from '../../models/Recipe.js';
 import { ErrorWithStatus } from '../../types/error.js';
+import { User } from '../../models/User.js';
 
 export default async (req: AuthenticatedReqBody<NewRecipeShareData>, 
   res: Response, next: NextFunction) => {
@@ -10,6 +11,10 @@ export default async (req: AuthenticatedReqBody<NewRecipeShareData>,
     const recipe = await Recipe.findById(req.body.recipeId);
     if (!recipe) {
       throw new ErrorWithStatus('Recipe not found', 404);
+    }
+    const user = await User.findById(req.body.userId);
+    if (!user) {
+      throw new ErrorWithStatus('User not found', 404);
     }
 
     if (recipe.ownerId != req.user.id) {
--- tests/recipe-shares.test.ts
+++ tests/recipe-shares.test.ts
@@ -83,7 +83,40 @@ describe('POST /recipe-shares tests', () => {
 
     expect(res.status).toBe(403);
     expect(res.body.message).toEqual('You are not authorized to share this recipe');
+  });
 
+  it('gives a 401 error for unauthenticated user', async () => {
+    const { agent, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .send({ userId: secondUser.id, recipeId, editable: true });
+
+    expect(res.status).toBe(401);
+    expect(res.body.message).toEqual('You must be signed in to continue');
+  });
+
+  it('gives a 404 error for nonexistent recipe', async () => {
+    const { agent, token } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: secondUser.id, recipeId: '794921', editable: false });
+
+    expect(res.status).toBe(404);
+    expect(res.body.message).toEqual('Recipe not found');
+  });
+
+  it('gives a 404 error for nonexistent user', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: '358', recipeId, editable: true });
+
+    expect(res.status).toBe(404);
+    expect(res.body.message).toEqual('User not found');
   });
 });
 
