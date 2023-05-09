03b2b9c29943cb5d9544d848970f79fb122487d3
Dillon Brock <mdillon1297@gmail.com>
2023-05-09 11:26:37 -0700

    passing tests for error cases on DELETE /recipe-shares/:id

--- tests/recipe-shares.test.ts
+++ tests/recipe-shares.test.ts
@@ -305,4 +305,24 @@ describe('DELETE /recipe-shares/:id tests', () => {
       }
     });
   });
+  
+  it('gives a 401 error for unauthenticated user', async () => {
+    const { agent, shareId } = await signUpAndShareRecipe();
+
+    const res = await agent.delete(`/recipe-shares/${shareId}`);
+    expect(res.status).toBe(401);
+    expect(res.body.message).toEqual('You must be signed in to continue');
+  });
+
+  it('gives a 403 error for unauthorized user', async () => {
+    const { agent, token2, shareId } = await signUpAndShareRecipe();
+
+    const res = await agent.delete(`/recipe-shares/${shareId}`)
+      .set('Authorization', `Bearer ${token2}`);
+
+    expect(res.status).toBe(403);
+    expect(res.body.message).toEqual(
+      'You are not authorized to make changes to this information'
+    );
+  });
 });
