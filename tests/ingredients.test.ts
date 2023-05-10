10b4e5d4da39b932c78d691f2e33fc321c014929
Dillon Brock <mdillon1297@gmail.com>
2023-05-09 20:30:04 -0700

    passing tests for error cases on DELETE /ingredients/:id

--- tests/ingredients.test.ts
+++ tests/ingredients.test.ts
@@ -333,4 +333,56 @@ describe('DELETE /ingredients/:id', () => {
     const ingredient = await Ingredient.findById(ingredientId);
     expect(ingredient).toBe(null);
   });
+
+  it('deletes an ingredient for user with edit access', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const { token2, secondUserId } = await createSecondaryUser(agent);
+    const ingredientId = await addIngredient(agent, token, recipeId);
+
+    await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: secondUserId, recipeId, editable: true });
+
+    const res = await agent.delete(`/ingredients/${ingredientId}`)
+      .set('Authorization', `Bearer ${token2}`);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: 'Ingredient deleted successfully',
+      ingredient: {
+        ...testIngredient,
+        id: ingredientId,
+        recipeId
+      }
+    });
+
+    const ingredient = await Ingredient.findById(ingredientId);
+    expect(ingredient).toBe(null);
+  });
+
+  it('gives a 403 error for unauthorized user', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const { token2, secondUserId } = await createSecondaryUser(agent);
+    const ingredientId = await addIngredient(agent, token, recipeId);
+
+    await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: secondUserId, recipeId, editable: false });
+
+    const res = await agent.delete(`/ingredients/${ingredientId}`)
+      .set('Authorization', `Bearer ${token2}`);
+
+    expect(res.status).toBe(403);
+    expect(res.body.message).toEqual('You are not authorized to edit this recipe');
+  });
+
+  it('gives a 401 error for unauthenticated user', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const ingredientId = await addIngredient(agent, token, recipeId);
+
+    const res = await agent.delete(`/ingredients/${ingredientId}`);
+
+    expect(res.status).toBe(401);
+    expect(res.body.message).toEqual('You must be signed in to continue');
+  });
 });
