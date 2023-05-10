b0f68fb00547eb8deba05b219ecfa756471eb0d0
Dillon Brock <mdillon1297@gmail.com>
2023-05-09 19:39:20 -0700

    passing unit tests for error cases at PUT /ingredients/:id

--- tests/ingredients.test.ts
+++ tests/ingredients.test.ts
@@ -166,4 +166,68 @@ describe('PUT /ingredients/:id', () => {
       }
     });
   });
+
+  it('updates an ingredient for user with edit access', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const { token2, secondUserId } = await createSecondaryUser(agent);
+    const ingredientId = await addIngredient(agent, token, recipeId);
+
+    await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: secondUserId, recipeId, editable: true });
+
+    const res = await agent.put(`/ingredients/${ingredientId}`)
+      .set('Authorization', `Bearer ${token2}`)
+      .send({ amount: '2 cups' });
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      message: 'Ingredient updated successfully',
+      ingredient: {
+        id: ingredientId,
+        name: 'milk',
+        amount: '2 cups',
+        recipeId
+      }
+    });
+  });
+
+  it('gives a 403 error for unauthorized user', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const { token2, secondUserId } = await createSecondaryUser(agent);
+    const ingredientId = await addIngredient(agent, token, recipeId);
+
+    await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: secondUserId, recipeId, editable: false });
+
+    const res = await agent.put(`/ingredients/${ingredientId}`)
+      .set('Authorization', `Bearer ${token2}`)
+      .send({ amount: '2 cups' });
+
+    expect(res.status).toBe(403);
+    expect(res.body.message).toEqual('You are not authorized to edit this recipe');
+  });
+
+  it('gives a 401 error for unauthenticated user', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const ingredientId = await addIngredient(agent, token, recipeId);
+
+    const res = await agent.put(`/ingredients/${ingredientId}`)
+      .send({ amount: '2 cups' });
+
+    expect(res.status).toBe(401);
+    expect(res.body.message).toEqual('You must be signed in to continue');
+  });
+
+  it('gives a 404 error for nonexistent ingredient', async () => {
+    const { agent, token } = await signUpAndCreateRecipe();
+
+    const res = await agent.put('/ingredients/568902')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'oat milk' });
+
+    expect(res.status).toBe(404);
+    expect(res.body.message).toEqual('Ingredient not found');
+  });
 });
