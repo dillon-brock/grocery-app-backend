41746bfb636181e50d1a6e7786347e7ee01ca982
Dillon Brock <mdillon1297@gmail.com>
2023-05-24 20:29:57 -0400

    passing tests for payload validation on PUT /list-items/:id

--- tests/list-items/update.test.ts
+++ tests/list-items/update.test.ts
@@ -4,7 +4,7 @@ import { UserService } from '../../lib/services/UserService.js';
 describe('PUT /list-items/:id tests', () => {
   beforeEach(setupDb);
 
-  test('it updates an item at PUT /list-items/:id', async () => {
+  it('it updates an item at PUT /list-items/:id', async () => {
     const { agent, token } = await signUpAndGetInfo();
     const { listId, categoryId } = await createListWithCategory(agent, token);
     const newItemId = await getNewItemId(agent, token, listId, categoryId);
@@ -25,7 +25,7 @@ describe('PUT /list-items/:id tests', () => {
     });
   });
 
-  test('it updates an item for shared user with edit access', async () => {
+  it('it updates an item for shared user with edit access', async () => {
     const { agent, token } = await signUpAndGetInfo();
     const { listId, categoryId } = await createListWithCategory(agent, token);
     const newItemId = await getNewItemId(agent, token, listId, categoryId);
@@ -80,7 +80,7 @@ describe('PUT /list-items/:id tests', () => {
     expect(res.body.message).toEqual('You are not authorized to access this item');
   });
 
-  test('gives a 404 error for nonexistent item', async () => {
+  it('gives a 404 error for nonexistent item', async () => {
     const { agent, token } = await signUpAndGetInfo();
     const res = await agent
       .put('/list-items/38942')
@@ -91,7 +91,7 @@ describe('PUT /list-items/:id tests', () => {
     expect(res.body.message).toEqual('List item not found');
   });
 
-  test('gives 401 error for unauthenticated user', async () => {
+  it('gives 401 error for unauthenticated user', async () => {
     const { agent, token } = await signUpAndGetInfo();
     const { listId, categoryId } = await createListWithCategory(agent, token);
     const newItemId = await getNewItemId(agent, token, listId, categoryId);
@@ -104,7 +104,7 @@ describe('PUT /list-items/:id tests', () => {
     expect(res.body.message).toEqual('You must be signed in to continue');
   });
 
-  test('gives a 403 error for unauthorized user', async () => {
+  it('gives a 403 error for unauthorized user', async () => {
     const { agent, token } = await signUpAndGetInfo();
     const { listId, categoryId } = await createListWithCategory(agent, token);
     const newItemId = await getNewItemId(agent, token, listId, categoryId);
@@ -121,5 +121,75 @@ describe('PUT /list-items/:id tests', () => {
 
     expect(res.status).toBe(403);
     expect(res.body.message).toEqual('You are not authorized to access this item');
+  });
+
+  it('gives a 400 error for invalid argument', async () => {
+    const { agent, token } = await signUpAndGetInfo();
+    const { listId, categoryId } = await createListWithCategory(agent, token);
+    const newItemId = await getNewItemId(agent, token, listId, categoryId);
+  
+    const res = await agent
+      .put(`/list-items/${newItemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ badData: '' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - unexpected argument badData');
+  });
+
+  it('gives a 400 error for invalid type (item)', async () => {
+    const { agent, token } = await signUpAndGetInfo();
+    const { listId, categoryId } = await createListWithCategory(agent, token);
+    const newItemId = await getNewItemId(agent, token, listId, categoryId);
+  
+    const res = await agent
+      .put(`/list-items/${newItemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ item: 2 });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - item must be string or omitted');
+  });
+
+  it('gives a 400 error for invalid type (category_id)', async () => {
+    const { agent, token } = await signUpAndGetInfo();
+    const { listId, categoryId } = await createListWithCategory(agent, token);
+    const newItemId = await getNewItemId(agent, token, listId, categoryId);
+  
+    const res = await agent
+      .put(`/list-items/${newItemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ category_id: {} });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - category_id must be string or omitted');
+  });
+
+  it('gives a 400 error for invalid type (quantity)', async () => {
+    const { agent, token } = await signUpAndGetInfo();
+    const { listId, categoryId } = await createListWithCategory(agent, token);
+    const newItemId = await getNewItemId(agent, token, listId, categoryId);
+  
+    const res = await agent
+      .put(`/list-items/${newItemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ quantity: 5 });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - quantity must be string or null or omitted');
+  });
+
+  it('gives a 400 error for invalid type (bought)', async () => {
+    const { agent, token } = await signUpAndGetInfo();
+    const { listId, categoryId } = await createListWithCategory(agent, token);
+    const newItemId = await getNewItemId(agent, token, listId, categoryId);
+  
+    const res = await agent
+      .put(`/list-items/${newItemId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ bought: 'true' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - bought must be boolean or omitted');
   });
 });
