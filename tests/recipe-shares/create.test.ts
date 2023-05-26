f89968eb490f2670bdf119bb91993b5b8a1edcb7
Dillon Brock <mdillon1297@gmail.com>
2023-05-26 13:32:46 -0400

    POST /recipe-shares validation passing tests

--- tests/recipe-shares/create.test.ts
+++ tests/recipe-shares/create.test.ts
@@ -1,4 +1,4 @@
-import { setupDb, signUpAndCreateRecipe, testUser2, testUser3 } from '../utils.js';
+import { setupDb, signUp, signUpAndCreateRecipe, testUser2, testUser3 } from '../utils.js';
 import { UserService } from '../../lib/services/UserService.js';
 
 describe('POST /recipe-shares tests', () => {
@@ -71,5 +71,100 @@ describe('POST /recipe-shares tests', () => {
 
     expect(res.status).toBe(404);
     expect(res.body.message).toEqual('User not found');
+  });
+
+  it('gives a 400 error for missing recipeId', async () => {
+    const { agent, token } = await signUp();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ userId: secondUser.id, editable: false });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - recipeId is required');
+  });
+
+  it('gives a 400 error for missing userId', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId, editable: false });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - userId is required');
+  });
+
+  it('gives a 400 error for missing editable', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId, userId: secondUser.id });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - editable is required');
+  });
+
+  it('gives a 400 error for invalid recipeId type', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId: Number(recipeId), userId: secondUser.id, editable: true });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - recipeId must be string');
+  });
+
+  it('gives a 400 error for invalid recipeId type', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId: Number(recipeId), userId: secondUser.id, editable: true });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - recipeId must be string');
+  });
+
+  it('gives a 400 error for invalid userId type', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId, userId: Number(secondUser.id), editable: true });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - userId must be string');
+  });
+
+  it('gives a 400 error for invalid editable type', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId, userId: secondUser.id, editable: 'true' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - editable must be boolean');
+  });
+
+  it('gives a 400 error for extra arguments', async () => {
+    const { agent, token, recipeId } = await signUpAndCreateRecipe();
+    const secondUser = await UserService.create(testUser2);
+
+    const res = await agent.post('/recipe-shares')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ recipeId, userId: secondUser.id, editable: true, other: 'bad data' });
+
+    expect(res.status).toBe(400);
+    expect(res.body.message).toEqual('Invalid payload - unexpected argument other');
   });
 });
