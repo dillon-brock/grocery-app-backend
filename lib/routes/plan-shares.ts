e31aa27cad868fb7ddd420fdc94650912e6aa919
Dillon Brock <mdillon1297@gmail.com>
2023-06-11 16:22:19 -0700

    payload validation and authorization middleware for POST /plan-shares

--- lib/routes/plan-shares.ts
+++ lib/routes/plan-shares.ts
@@ -4,16 +4,20 @@ import { AuthenticatedReqBody, TypedResponse } from '../types/extendedExpress.js
 import { NewPlanShareData, PlanShareRes } from '../types/planShare.js';
 import { NextFunction } from 'express-serve-static-core';
 import { PlanShare } from '../models/PlanShare.js';
+import validateSharePlan from '../middleware/validation/plan-shares/create.js';
+import authorizeSharePlan from '../middleware/authorization/plan-shares/create.js';
 
 export default Router()
-  .post('/', authenticate, async (req: AuthenticatedReqBody<NewPlanShareData>, res: TypedResponse<PlanShareRes>, next: NextFunction) => {
-    try {
-      const planShare = await PlanShare.create(req.body);
-      res.json({
-        message: 'Meal plan shared successfully',
-        planShare
-      });
-    } catch (e) {
-      next(e);
-    }
-  });
+  .post('/', [authenticate, validateSharePlan, authorizeSharePlan], 
+    async (req: AuthenticatedReqBody<NewPlanShareData>, 
+      res: TypedResponse<PlanShareRes>, next: NextFunction) => {
+      try {
+        const planShare = await PlanShare.create(req.body);
+        res.json({
+          message: 'Meal plan shared successfully',
+          planShare
+        });
+      } catch (e) {
+        next(e);
+      }
+    });
